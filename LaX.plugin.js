d5878b6feabd48d4de7ea71923adf0bf5478561b
Josefifir <hagbardoldman@gmail.com>
2025-04-19 13:27:04 +0200

    Update LaX.plugin.js

--- LaX.plugin.js
+++ LaX.plugin.js
@@ -14,10 +14,26 @@ MIT License
 
 Copyright (c) 2025-2026 Nothing
 
-c
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 */
 
-// Updated March 29th, 2025.
+// Updated April 19th, 2025.
 
 
 
@@ -27166,7 +27182,7 @@ class TeX {
           name: "LaX", // Plugin name
           authors: [
               {
-                  name: "The Real Kozel",
+                  name: "Nothing",
                   discord_id: "1278640580607479851",
                   github_username: "Josefifir"
               }
@@ -27174,7 +27190,7 @@ class TeX {
           version: "1.0.0",
           description: "Creates and sends TeX math equations.",
           github: "https://github.com/Josefifir/TeX",
-          github_raw: "https://raw.githubusercontent.com/Josefifir/TeX/main/TeX.plugin.js"
+          github_raw: "https://raw.githubusercontent.com/Josefifir/TeX/TeX.plugin.js"
       },
       defaultConfig: [
           {
